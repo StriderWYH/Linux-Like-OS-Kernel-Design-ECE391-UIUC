cf694b0b12df342c0f465619a9648aa800478d93
FREDPLANC <1310737319@qq.com>
2022-11-06 09:36:30 -0600

    debug finished ckp3

--- student-distrib/systemcall.c
+++ student-distrib/systemcall.c
@@ -61,12 +61,17 @@ int32_t current_pid = -1;
 
 
 
+/*
+ *  execute
+ *  DESCRIPTION: execute a file
+ *  INPUTS:            command: the command to read                
+ *  OUTPUTS:            none
 
-// int32_t (*file_table[3][4])() = {
-// {RTC_read, RTC_write, RTC_open, RTC_close},
-// {dir_read, dir_write, dir_open, dir_close},
-// {file_read, file_write, file_open, file_close}
-// };
+ *  SIDEEFFECT: NONE
+ *  RETURN VALUE:       0 normal success 
+ *                      -1 wrong
+ */
+
 int32_t execute(const uint8_t* command) {
     //puts("executes called \n");
     // 1. parse arguments
@@ -239,6 +244,16 @@ int32_t execute(const uint8_t* command) {
     );
     return 0;
 }
+/*
+ *  getargs
+ *  DESCRIPTION: get argument from the pcb args
+ *  INPUTS:             buf to fill the args; nbytes: number of bytes to read                
+ *  OUTPUTS:            none
+
+ *  SIDEEFFECT: NONE
+ *  RETURN VALUE:       0 normal success
+ *                      -1 wrong
+ */
 
 int32_t getargs(uint8_t* buf, int32_t nbytes){
     int i = 0;
